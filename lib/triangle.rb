ec76cb4177cc114f12379772b5562e6929f52c85
Nicole Lucien <nicolemlucien@gmail.com>
2019-11-11 12:54:12 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,7 @@ class Triangle
   def kind 
     if side1 == side2 && side2 == side3
       "equilateral"
-    elsif side1 != side2 != side3
+    elsif side1 != side2 && side2 != side3
       "scalene"
     else 
       "isosceles"
