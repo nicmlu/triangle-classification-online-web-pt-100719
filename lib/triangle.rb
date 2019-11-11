1eeb54d53b33472783a102baa92875be054a33e5
Nicole Lucien <nicolemlucien@gmail.com>
2019-11-11 13:06:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
     if side1 == side2 && side2 == side3 && side1 == side3
       p :equilateral
     elsif side1 != side2 && side2 != side3 && side1 != side3
-    && side1 > 0 && side2 > 0 && side3 > 0 
+    side1 > 0 && side2 > 0 && side3 > 0 
       p :scalene
     elsif side1 == side3 || side2 == side3 || side1 == side2
       p :isosceles
