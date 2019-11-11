b8c05e0ed3a6dc9be76eff28219b1f78e6d441af
Nicole Lucien <nicolemlucien@gmail.com>
2019-11-11 13:06:50 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,6 +9,7 @@ class Triangle
   
   def kind 
     if side1 == side2 && side2 == side3 && side1 == side3
+      side1 > 0 && side2 > 0 && side3 > 0
       p :equilateral
     elsif side1 != side2 && side2 != side3 && side1 != side3
     side1 > 0 && side2 > 0 && side3 > 0 
