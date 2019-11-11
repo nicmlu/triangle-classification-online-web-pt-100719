491575f0d72e33c3ff1567f3211dc4ca4c56f6eb
Nicole Lucien <nicolemlucien@gmail.com>
2019-11-11 13:21:16 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -29,7 +29,10 @@ class Triangle
   def invalid?
    if side1 <= 0 || side2 <= 0 || side3 <= 0
      true
-   elsif side1 + side2 <= side3 || side
+   elsif side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2
+    true
+  else
+    false
   end 
   
 end
